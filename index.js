a9baca9b4527cfb0d06e7d52158fd5c76304658f
jhuang429 <jhuang9240@gmail.com>
2020-02-17 18:39:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+function findMatching(arr, str) {  
+    return arr.filter(driver => driver.toLowerCase() === str.toLowerCase() );
+}
+
+function fuzzyMatch(arr, str) {
+    let newregex = new RegExp(str)
+    let strLength = str.length
+
+    return arr.filter( driver => 
+        driver.slice(0, strLength).match(newregex)
+        )
+}
+
+function matchName(arr, str) {
+    return arr.filter(driver => driver.name === str )
+}
